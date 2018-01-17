148e95d9a470fa316b975640d38978a4903b41d6
Yann VILLEPONTOUX <yvillepo@e2r8p18.42.fr>
2018-01-17 10:59:19 +0100

    modif proprete init

--- fractol/src/init.c
+++ fractol/src/init.c
@@ -6,12 +6,25 @@
 /*   By: yvillepo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 00:42:44 by yvillepo          #+#    #+#             */
-/*   Updated: 2018/01/13 00:59:45 by yvillepo         ###   ########.fr       */
+/*   Updated: 2018/01/17 10:57:09 by yvillepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 #include "libft.h"
+
+void	init_mlx(t_mlx **mlx)
+{
+	t_mlx	*m;
+
+	*mlx = ft_memalloc(sizeof(*m));
+	m = *mlx;
+	if (!((m->mlx = mlx_init())
+				&& (m->win = mlx_new_window(m->mlx,
+						MAX_WIDTH, MAX_HEIGHT, "mlx 42"))))
+		exit_error("init");
+	m->image.im = new_image(m, &(m->image.pim));
+}
 
 t_mlx	*init(int ac, char **av)
 {
@@ -20,10 +33,8 @@ t_mlx	*init(int ac, char **av)
 	if (ac < 2)
 		exit_error("nb arg");
 	m = ft_memalloc(sizeof(*m));
-	if (!((m->mlx = mlx_init())
-				&& (m->win = mlx_new_window(m->mlx,
-						MAX_WIDTH, MAX_HEIGHT, "mlx 42"))))
-		exit_error("init");
-	m->image.im = new_image(m, &(m->image.pim));
+	init_mlx(&m);
+//	m->width = DEFAULT_WIDTH;
+//	m->height = DEFAULT_HEIGHT;
 	return (m);
 }
